ab1f946c7cffab3cfbcf4e6ba3da1b2ed95cfefc
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-10-29 21:36:04 -0300

    add holidays

--- application/main.py
+++ application/main.py
@@ -1,5 +1,7 @@
 from datetime import date, timedelta, datetime
 
+import holidays as holidays
+
 start_date = datetime.strptime(input('Data inicial (dd/mm/year):'), '%d/%m/%Y').date()
 end_date = datetime.strptime(input('Data final (dd/mm/year):'), '%d/%m/%Y').date()
 # date_final = input('Data final (dd/mm/year)')
@@ -11,3 +13,8 @@ def daterange(start_date, end_date):
 
 for single_date in daterange(start_date, end_date):
     print(single_date.strftime("%d-%m-%Y"))
+
+br_holidays = holidays.Brazil(state='SE')
+# date(year, mm, dd) in custom_holidays # return true or false
+
+# dia_semana = calendar.weekday(ano, mes, dia) # return a number of week
